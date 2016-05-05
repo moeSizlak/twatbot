2902b6ed356c53f8c7d2f86f56b4b236b9b3ce77
moeSizlak <paul14075@gmail.com>
2016-05-05 01:55:32 +0100

    fix indentation

--- bot3config.rb
+++ bot3config.rb
@@ -1,19 +1,19 @@
 
 module MyApp
-  module Config
-    SERVER = "server.irc.org"
-	PORT = 38173
-    CHANNELS = ["#FU1","#ORLY","#yaRLY","#New_World_Order","#testing12"]
-    USER = "twatbot"
-    PASSWORD = "KILLKILLKILL"
-    SSL = true
-    NICK = "twatbot"
-	
-	GOOGLE_SERVER_KEY = "HDJHDSJKHDKSJGDKGHSKHDKSHKDHSKHDKSHKJDH"
-	
-	TB_SQL_SERVER = '127.0.0.1'
-	TB_SQL_USER = 'god'
-	TB_SQL_PASSWORD = 'thisisnotarealpasswordyouassclown'
-	TB_SQL_DATABASE = "leet"
-  end
+	module Config
+		SERVER = "server.irc.org"
+		PORT = 38173
+		CHANNELS = ["#FU1","#ORLY","#yaRLY","#New_World_Order","#testing12"]
+		USER = "twatbot"
+		PASSWORD = "KILLKILLKILL"
+		SSL = true
+		NICK = "twatbot"
+
+		GOOGLE_SERVER_KEY = "HDJHDSJKHDKSJGDKGHSKHDKSHKDHSKHDKSHKJDH"
+
+		TB_SQL_SERVER = '127.0.0.1'
+		TB_SQL_USER = 'god'
+		TB_SQL_PASSWORD = 'thisisnotarealpasswordyouassclown'
+		TB_SQL_DATABASE = "leet"
+	end
 end
