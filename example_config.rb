9aaa18452ba194f84fd56a69979fae84f1da0212
moeSizlak <paul14075@gmail.com>
2016-05-10 00:23:04 +0100

    major twatbot update to modularize and add config file parameter

--- example_config.rb
+++ example_config.rb
@@ -1,19 +1,53 @@
 
 module MyApp
-	module Config
-		SERVER = "server.irc.org"
-		PORT = 38173
-		CHANNELS = ["#FU1","#ORLY","#yaRLY","#New_World_Order","#testing12"]
-		USER = "twatbot"
-		PASSWORD = "KILLKILLKILL"
-		SSL = true
-		NICK = "twatbot"
-
-		GOOGLE_SERVER_KEY = "HDJHDSJKHDKSJGDKGHSKHDKSHKDHSKHDKSHKJDH"
-
-		TB_SQL_SERVER = '127.0.0.1'
-		TB_SQL_USER = 'god'
-		TB_SQL_PASSWORD = 'thisisnotarealpasswordyouassclown'
-		TB_SQL_DATABASE = "leet"
-	end
+  module Config
+    IRC_SERVER = "server.irc.com"
+	IRC_PORT = 7777
+    IRC_CHANNELS = ["#chan1","#test1","#etc"]
+    IRC_USER = "username"
+    IRC_PASSWORD = "password"
+    IRC_SSL = true
+    IRC_NICK = "thebot"
+	IRC_PLUGINS = [
+			"Plugins::RSSFeed",
+			"Plugins::URL",
+			"Plugins::TvMaze",
+			"Plugins::IMDB",
+			#"Plugins::URLDB",
+		]
+	
+	# Plugins::URL
+	URL_SUB_PLUGINS = [  # each one of these will be tried in order until one of them works:
+		{:class => "URLHandlers::Youtube",  :excludeChans => [], :excludeNicks => []},
+		{:class => "URLHandlers::IMDB",     :excludeChans => [], :excludeNicks => []},
+		{:class => "URLHandlers::Dumpert",  :excludeChans => [], :excludeNicks => []},
+		{:class => "URLHandlers::TitleBot", :excludeChans => [], :excludeNicks => []},
+		]
+		
+	YOUTUBE_GOOGLE_SERVER_KEY = "YOUR_GOOGLE_SERVER_KEY_FOR_YOUTUBE_API"
+		
+	# Plugins::IMDB
+	IMDB_EXCLUDE_CHANS = []
+	IMDB_EXCLUDE_USERS = []
+	
+	# Plugins::TvMaze
+	TVMAZE_EXCLUDE_CHANS = []
+	TVMAZE_EXCLUDE_USERS = []
+	
+	# Plugins::RSSFeed
+	RSS_FEEDS = [
+		{:name => "TorrentFreak", :url => "http://feeds.feedburner.com/Torrentfreak", :chans => ["#test1"], :old => nil},  
+		#{:name => "Slashdot", :url => "http://rss.slashdot.org/Slashdot/slashdotMain", :chans => ["#test1"], :old => nil},
+		]
+	
+	# Plugins::URLDB
+	URLDB_CHANS = []
+	URLDB_EXCLUDE_USERS = []
+	URLDB_SQL_SERVER = '127.0.0.1'
+	URLDB_SQL_USER = 'yoursqlusername'
+	URLDB_SQL_PASSWORD = 'yoursqlpassword'
+	URLDB_SQL_DATABASE = "yoursqldatabase"
+	
+  end
 end
+
