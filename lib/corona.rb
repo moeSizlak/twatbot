45beaaedb98ebe3b0b5380c45fbbfe07cea4a4a2
moeSizlak <paul14075@gmail.com>
2020-03-12 22:21:02 -0400

    meh

--- lib/corona.rb
+++ lib/corona.rb
@@ -16,11 +16,11 @@ module Plugins
     
     match /^!(?:help|commands)/, use_prefix: false, method: :help
     #match lambda {|m| /^\.(?!btc)(#{m.bot.botconfig[:COINS].map{|x| Regexp.escape(x["symbol"])}.join('|')})\s*$/im}, use_prefix: false, method: :getCorona
-    match /^\.corona(\s*|\s+[\S\s]+\S\s*)$/im, use_prefix: false, method: :getCorona2
+    match /^\.corona(\s*|\s+[\S\s]+\S\s*)$/im, use_prefix: false, method: :getCorona
     match /^\.coronaupdate/im, use_prefix: false, method: :updatecorona2
 
     #timer 0,  {:method => :updatecorona, :shots => 1}
-    #timer 60, {:method => :updatecorona}  
+    timer 3600, {:method => :updatecorona2}  
 
     def initialize(*args)
       super
@@ -33,6 +33,7 @@ module Plugins
 
       @CountryCacheEntry = Class.new(Sequel::Model(bot.botconfig[:DB][:countries]))
       @CountryCacheEntry.unrestrict_primary_key
+
 
       @airports = CSV.read(File.dirname(__FILE__) + "/airports_large.txt")
 
@@ -50,7 +51,12 @@ module Plugins
 
     def updatecorona
       #mycorona = Unirest::get("https://api.coinmarketcap.com/v1/ticker/?limit=0") rescue nil
-      mycorona = Unirest::get("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&resultOffset=0&resultRecordCount=200&cacheHint=true", headers:{"authority" => "services1.arcgis.com", "pragma" => "no-cache", "cache-control" => "no-cache", "user-agent" => "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36", "sec-fetch-dest" => "empty", "accept" => "*/*", "origin" => "https://gisanddata.maps.arcgis.com", "sec-fetch-site" => "same-site", "sec-fetch-mode" => "cors", "referer" => "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html", "accept-language" => "en-US,en;q=0.9"}) rescue nil
+      #mycorona = Unirest::get("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/2/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&resultOffset=0&resultRecordCount=200&cacheHint=true", headers:{"authority" => "services1.arcgis.com", "pragma" => "no-cache", "cache-control" => "no-cache", "user-agent" => "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36", "sec-fetch-dest" => "empty", "accept" => "*/*", "origin" => "https://gisanddata.maps.arcgis.com", "sec-fetch-site" => "same-site", "sec-fetch-mode" => "cors", "referer" => "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html", "accept-language" => "en-US,en;q=0.9"}) rescue nil
+      mycorona = Unirest::get("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/query?f=json&where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc%2CCountry_Region%20asc%2CProvince_State%20asc&resultOffset=0&resultRecordCount=250&cacheHint=true", headers:{"authority" => "services1.arcgis.com", "pragma" => "no-cache", "cache-control" => "no-cache", "user-agent" => "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36", "sec-fetch-dest" => "empty", "accept" => "*/*", "origin" => "https://gisanddata.maps.arcgis.com", "sec-fetch-site" => "same-site", "sec-fetch-mode" => "cors", "referer" => "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html", "accept-language" => "en-US,en;q=0.9"}) rescue nil
+      
+
+
+
 
       if !mycorona.nil? && !mycorona.body.nil? && !mycorona.body["features"].nil?
           @@corona = mycorona.body["features"]
@@ -76,6 +82,11 @@ module Plugins
       datesNeed.each do |d|
         dl = Unirest::get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/#{ Date.parse(d).strftime("%m-%d-%Y") }.csv")
         if dl.body =~ /^(\xEF\xBB\xBF)?Province/
+
+          open(File.dirname(__FILE__) + "/corona/#{ Date.parse(d).strftime("%m-%d-%Y") }.csv", "wb") do |file|
+            file.write(dl.body)
+          end
+
           csv = CSV.parse(dl.body, headers: true)
           csv.each do |row|
             c = @CoronaEntry.new
@@ -237,28 +248,67 @@ module Plugins
 
 
 
-        def getCorona(m,c)
+    def getCorona(m,c)
       if m.bot.botconfig[:CORONA_EXCLUDE_CHANS].map(&:downcase).include?(m.channel.to_s.downcase) || m.bot.botconfig[:CORONA_EXCLUDE_USERS].map(&:downcase).include?(m.user.to_s.downcase)
         return
       end
 
-      c.strip!
+      color_confirmed = "07"
+      color_deaths = "07"
+      color_recovered = "07"
 
+      c.strip!
+      puts "AAAAAAAA"
       @@corona_mutex.synchronize do
+        puts "BBBBBBBBB"
         updatecorona if (@@corona_lastupdate.nil? || (@@corona_lastupdate < (DateTime.now - (15/1440.0))))
+        puts "CCCCCCCCCC"
+        if @@corona.nil?
+          m.reply "API Unavailable" 
+          getCorona2(m, c)
+          return
+        end
 
         if c.nil? || c.length == 0
+          confirmed = @@corona.map{|x| x["attributes"]["Confirmed"]}.inject(0, :+)
+          deaths = @@corona.map{|x| x["attributes"]["Deaths"]}.inject(0, :+)
+          recovered = @@corona.map{|x| x["attributes"]["Recovered"]}.inject(0, :+)
+
           m.reply "" +
-          "\x03".b + "04" + "CORONA!:" + "\x0f".b + " #{@@corona.map{|x| x["attributes"]["Confirmed"]}.inject(0, :+).to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Confirmed | #{@@corona.map{|x| x["attributes"]["Deaths"]}.inject(0, :+).to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Deaths | #{@@corona.map{|x| x["attributes"]["Recovered"]}.inject(0, :+).to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Recovered" +
+         "\x03".b + "04" + "CORONA!!!" + "\x0f".b + "\x03".b + color_confirmed + "  Confirmed:" + "\x0f".b + " #{confirmed.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} | "  + "\x03".b + color_deaths + "Deaths:" + "\x0f".b + " #{deaths.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*deaths.to_f/confirmed.to_f)} %) | " + "\x03".b + color_recovered + "Recovered:" + "\x0f".b + " #{recovered.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*recovered.to_f/confirmed.to_f)} %)" +
           (m.channel.to_s.downcase == "#testing12" ? " [#{@@corona_lastupdate}]" : "")
           return
 
         else
         
           cc = nil
-          cc = @@corona.find{|x| x["attributes"]["Country_Region"].upcase == c.upcase}
+          cc = @@corona.find{|x| (x["attributes"]["Country_Region"] || "").upcase == c.upcase}
+
+          if cc.nil?
+            cc = @@corona.find{|x| (x["attributes"]["Province_State"] || "").upcase == c.upcase}
+            if cc.nil?
+              mystate = @config[:DB][:states].where(Sequel.function(:upper, :code) => c.upcase).all
+              if mystate && mystate.count == 1
+                cc = @@corona.find{|x| (x["attributes"]["Province_State"] || "").upcase == mystate.first[:name].upcase}
+              end
+            end
+
+            if !cc.nil?
+              confirmed = @@corona.select{|x| (x["attributes"]["Province_State"] || "").upcase == cc["attributes"]["Province_State"].upcase}.map{|x| x["attributes"]["Confirmed"]}.inject(0, :+)
+              deaths = @@corona.select{|x| (x["attributes"]["Province_State"] || "").upcase == cc["attributes"]["Province_State"].upcase}.map{|x| x["attributes"]["Deaths"]}.inject(0, :+)
+              recovered = @@corona.select{|x| (x["attributes"]["Province_State"] || "").upcase == cc["attributes"]["Province_State"].upcase}.map{|x| x["attributes"]["Recovered"]}.inject(0, :+)
+
+              m.reply "" +
+              "\x03".b + "04" + "#{cc["attributes"]["Province_State"]}:" + "\x0f".b + "\x03".b + color_confirmed + "  Confirmed:" + "\x0f".b + " #{confirmed.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} | "  + "\x03".b + color_deaths + "Deaths:" + "\x0f".b + " #{deaths.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*deaths.to_f/confirmed.to_f)} %) | " + "\x03".b + color_recovered + "Recovered:" + "\x0f".b + " #{recovered.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*recovered.to_f/confirmed.to_f)} %)" +
+
+              (m.channel.to_s.downcase == "#testing12" ? " [#{@@corona_lastupdate}]" : "")
+            end
+
+          end
+
 
           if cc.nil? # no exact match found in JHU DB, do geocoding lookup:
+            puts "Did not find exact corona location match for '#{c.upcase}'."
             mylocation = c.dup
 
             my_airport = @airports.find{|x| x[9].upcase == mylocation.upcase rescue nil}
@@ -276,6 +326,8 @@ module Plugins
               fad = c.display_name
               mycunt = c.country
               newloc = 1
+
+              puts "Found location lat/long/country/name in cache:  #{lat}/#{lng}/#{mycunt}/#{fad}"
 
               bot.botconfig[:DB][:location_cache].returning(:counter).where(:location => mylocation).update(:counter => Sequel.expr(1) + :counter)
 
@@ -316,9 +368,11 @@ module Plugins
               return
             end
 
+            puts "Searching corona country map for '#{mycunt}'"
             c = @CountryCacheEntry[mycunt]
             if c
-              cc = @@corona.find{|x| x["attributes"]["Country_Region"].upcase == c.jhu_code.upcase}
+              puts "Found JHU code of '#{c.jhu_code}'."
+              cc = @@corona.find{|x| (x["attributes"]["Country_Region"] || "").upcase == c.jhu_code.upcase}
               if cc.nil?
                 errormsg = "Failed. (#{c.jhu_code.upcase})"
                 botlog errormsg, m
@@ -336,11 +390,17 @@ module Plugins
 
 
 
-          c = cc["attributes"]
+          #c = cc["attributes"]
           #botlog "#{c["name"]} (#{c["symbol"]}) LU=#{@@corona_lastupdate}",m
+          puts "cc=#{cc}"
+          confirmed = @@corona.select{|x| (x["attributes"]["Country_Region"] || "").upcase == cc["attributes"]["Country_Region"].upcase}.map{|x| x["attributes"]["Confirmed"]}.inject(0, :+)
+          deaths = @@corona.select{|x| (x["attributes"]["Country_Region"] || "").upcase == cc["attributes"]["Country_Region"].upcase}.map{|x| x["attributes"]["Deaths"]}.inject(0, :+)
+          recovered = @@corona.select{|x| (x["attributes"]["Country_Region"] || "").upcase == cc["attributes"]["Country_Region"].upcase}.map{|x| x["attributes"]["Recovered"]}.inject(0, :+)
+
+
 
           m.reply "" +
-          "\x03".b + "04" + "#{c["Country_Region"]}:" + "\x0f".b + " #{c["Confirmed"].to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Confirmed | #{c["Deaths"].to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Deaths | #{c["Recovered"].to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} Recovered" +
+          "\x03".b + "04" + "#{cc["attributes"]["Country_Region"]}:" + "\x0f".b + "\x03".b + color_confirmed + "  Confirmed:" + "\x0f".b + " #{confirmed.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} | "  + "\x03".b + color_deaths + "Deaths:" + "\x0f".b + " #{deaths.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*deaths.to_f/confirmed.to_f)} %) | " + "\x03".b + color_recovered + "Recovered:" + "\x0f".b + " #{recovered.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse} (#{'%.2f' % (100*recovered.to_f/confirmed.to_f)} %)" +
 
           (m.channel.to_s.downcase == "#testing12" ? " [#{@@corona_lastupdate}]" : "")
 
